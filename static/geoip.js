28d6962b93aac4409e29e0bc85a0170430eae1b6
ionflare <ccionflare@gmail.com>
2018-11-08 17:57:46 +0700

    init master

--- static/geoip.js
+++ static/geoip.js
@@ -1,12 +1,12 @@
-var x = document.getElementById("demo");
+//var x = document.getElementById("demo");
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
     } else {
-        x.textContent = "Geolocation is not supported by this browser.";
+        document.getElementById("demo").textContent = "Geolocation is not supported by this browser.";
     }
 }
 function showPosition(position) {
-    x.textContent = "Latitude: " + position.coords.latitude + 
+    document.getElementById("demo").textContent = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
 }
