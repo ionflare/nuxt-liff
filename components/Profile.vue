ad89c35d768ea7778365daf3b93e50303a7fea43
ionflare <ccionflare@gmail.com>
2018-11-09 17:04:48 +0700

    change ui

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,16 +1,72 @@
 <template>
 
   <div>
-        <v-layout>
-        <div>
-          <v-btn onclick="getLocation()">Try GEOIP</v-btn>
+    <h1>Test Profile</h1>
 
-                <p id="demo"></p>
+      <div id="test"></div>
+
+
+      <div class="buttongroup">
+        <div class="buttonrow">
+            <v-btn id="openwindowbutton">Open Window</v-btn>
+            <v-btn id="closewindowbutton">Close Window</v-btn>
         </div>
-        </v-layout>
-        <v-layout>
-            <h1>Test User Profile</h1>
-         </v-layout>
-   
+        <div class="buttonrow">
+            <v-btn id="getprofilebutton">Get Profile</v-btn>
+            <v-btn id="sendmessagebutton">Send Message</v-btn>
+        </div>
+    </div>
+
+    <div id="profileinfo">
+        <h2>Profile</h2>
+        <a href="#" onclick="toggleProfileData()">Close Profile</a>
+        <div id="profilepicturediv">
+        </div>
+        <table border="1">
+            <tr>
+                <th>userId</th>
+                <td id="useridprofilefield"></td>
+            </tr>
+            <tr>
+                <th>displayName</th>
+                <td id="displaynamefield"></td>
+            </tr>
+            <tr>
+                <th>statusMessage</th>
+                <td id="statusmessagefield"></td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="liffdata">
+        <h2>LIFF Data</h2>
+        <table border="1">
+            <tr>
+                <th>language</th>
+                <td id="languagefield"></td>
+            </tr>
+            <tr>
+                <th>context.viewType</th>
+                <td id="viewtypefield"></td>
+            </tr>
+            <tr>
+                <th>context.userId</th>
+                <td id="useridfield"></td>
+            </tr>
+            <tr>
+                <th>context.utouId</th>
+                <td id="utouidfield"></td>
+            </tr>
+            <tr>
+                <th>context.roomId</th>
+                <td id="roomidfield"></td>
+            </tr>
+            <tr>
+                <th>context.groupId</th>
+                <td id="groupidfield"></td>
+            </tr>
+        </table>
+    </div>
+  
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,110 @@
 <template>
-    <div>
-        <v-card height="600px" flat>
-            <div class="headline text-xs-center pa-5">
-                Active: {{ bottomNav }}
-                <div v-if="bottomNav== 'profile' ">
 
-                    <v-container>
-                        <v-layer>
-                            <v-flex sm6 md6>
-                                <v-btn id='getprofilebutton' onclick="getprofile()">Show Profile Info</v-btn>
-                            </v-flex>
-                        </v-layer>
-                        <v-layer>
-                            <v-flex sm6 md6>
-                                <div id='profilepicturediv'></div>
-                            </v-flex>
-                        </v-layer>
-                        <v-layer>
-                            <v-flex sm6 md6>
-                                 <v-text id="useridfield"></v-text>
-                            </v-flex>
-                        </v-layer>
-                        <v-layer>
-                            <v-flex sm6 md6>
-                                 <v-text id="displaynamefield"></v-text>
-                            </v-flex>
-                        </v-layer>
-                        
-                    </v-container>
-                     
-
-
-
-                </div>
-                <div v-if="bottomNav== 'favorites' ">
-                    <v-container>
-                        <v-layer>
-                            <v-flex sm6 md6>
-                                 Show Your Current Position
-                            </v-flex>
-                            <v-flex sm6 md6>
-                                 <v-btn onclick="getLocation()">Try GEOIP</v-btn>
-                            </v-flex>
-                             <v-flex sm6 md6>
-                                <v-text id="demo"></v-text>
-                            </v-flex>
-                        </v-layer>
-                    </v-container>
-                </div>
-                <div v-if="bottomNav== 'nearby' ">
-                    Choose Your Soul Mate
-                      <v-container><v-layer>
-                         <SwipeUser v-bind="getProp" />
-                     </v-layer></v-container>
-                </div>
+    <v-container fluid=true>
+        <v-layer>
+            <v-frame>
+                <v-card height="700px" flat>
+                    <div class="headline text-xs-center pa-5">
+                        Active: {{ bottomNav }}
+                        <div v-if="bottomNav== 'profile' ">
+                             <v-container><v-layer>
+                                <Profile v-bind="getProp" />
+                            </v-layer></v-container>
+                            <!--
+                            <v-container>
+                                <v-layer>
+                                    <v-flex sm6 md6>
+                                        <v-btn id='getprofilebutton' onclick="getprofile()">Show Profile Info</v-btn>
+                                    </v-flex>
+                                </v-layer>
+                                <v-layer>
+                                    <v-flex sm6 md6>
+                                        <div id='profilepicturediv'></div>
+                                    </v-flex>
+                                </v-layer>
+                                <v-layer>
+                                    <v-flex sm6 md6>
+                                        <v-text id="useridfield"></v-text>
+                                    </v-flex>
+                                </v-layer>
+                                <v-layer>
+                                    <v-flex sm6 md6>
+                                        <v-text id="displaynamefield"></v-text>
+                                    </v-flex>
+                                </v-layer>
+                                
+                            </v-container>
+                            -->
 
 
 
-            </div>
-            <v-bottom-nav
-                :active.sync="bottomNav"
-                :value="true"
-                absolute
-                color="transparent"
-            >
-                <v-btn
-                color="teal"
-                flat
-                value="profile"
-                >
-                <span>Profile</span>
-                <v-icon>history</v-icon>
-                </v-btn>
-        
-                <v-btn
-                color="teal"
-                flat
-                value="favorites"
-                >
-                <span>Favorites</span>
-                <v-icon>favorite</v-icon>
-                </v-btn>
-        
-                <v-btn
-                color="teal"
-                flat
-                value="nearby"
-                >
-                <span>Nearby</span>
-                <v-icon>place</v-icon>
-                </v-btn>
-            </v-bottom-nav>
-        </v-card>
-    </div>
+                        </div>
+                        <div v-if="bottomNav== 'favorites' ">
+                            <v-container>
+                                <v-layer>
+                                    <v-flex sm6 md6>
+                                        Show Your Current Position
+                                    </v-flex>
+                                    <v-flex sm6 md6>
+                                        <v-btn onclick="getLocation()">Try GEOIP</v-btn>
+                                    </v-flex>
+                                    <v-flex sm6 md6>
+                                        <v-text id="demo"></v-text>
+                                    </v-flex>
+                                </v-layer>
+                            </v-container>
+                        </div>
+                        <div v-if="bottomNav== 'nearby' ">
+                            Choose Your Soul Mate
+                            <v-container><v-layer>
+                                <SwipeUser v-bind="getProp" />
+                            </v-layer></v-container>
+                        </div>
+
+
+
+                    </div>
+                    <v-bottom-nav
+                        :active.sync="bottomNav"
+                        :value="true"
+                        absolute
+                        color="transparent"
+                    >
+                        <v-btn
+                        color="teal"
+                        flat
+                        value="profile"
+                        >
+                        <span>Profile</span>
+                        <v-icon>history</v-icon>
+                        </v-btn>
+                
+                        <v-btn
+                        color="teal"
+                        flat
+                        value="favorites"
+                        >
+                        <span>Favorites</span>
+                        <v-icon>favorite</v-icon>
+                        </v-btn>
+                
+                        <v-btn
+                        color="teal"
+                        flat
+                        value="nearby"
+                        >
+                        <span>Nearby</span>
+                        <v-icon>place</v-icon>
+                        </v-btn>
+                    </v-bottom-nav>
+                </v-card>
+            </v-frame>
+        </v-layer>
+    </v-container>
 </template>
 
 <script>
-//import Profile from '~/components/Profile.vue';
+import Profile from '~/components/Profile.vue';
 import SwipeUser from '~/components/SwipeUser.vue';
 
 export default {
@@ -113,7 +121,7 @@ export default {
     bottomNav: 'profile'
   }),
    components:{
-        SwipeUser
+        Profile,SwipeUser
   },
       computed:{
         getProp(){
