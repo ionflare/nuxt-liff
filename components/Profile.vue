f720a82976fb829fc3c49ca9b0e736904fa0094c
ionflare <ccionflare@gmail.com>
2018-11-29 17:59:33 +0700

    change profile

--- components/Profile.vue
+++ components/Profile.vue
@@ -9,27 +9,29 @@
         >
           <v-layout row wrap>    
 
-    <v-flex xs12>
-              <v-card color="purple" class="white--text">
-                <v-layout row>
-                  <v-flex xs7>
-                    <v-card-title primary-title>
-                      <div>
-                        <div class="headline">{{$store.state.currentUser.line_displayName}}</div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
+       <v-flex xs12>
+              <v-card color="cyan darken-2" class="white--text">
+                <v-layout>
                   <v-flex xs5>
                     <v-img
-                      :src="$store.state.currentUser.line_pictureUrl"
+                      :src="$store.state.currentUser.line_displayName"
                       height="125px"
                       contain
                     ></v-img>
                   </v-flex>
+                  <v-flex xs7>
+                    <v-card-title primary-title>
+                      <div>
+                        <div class="headline">{{$store.state.currentUser.line_displayName}}</div>
+                        <div>statusMessage</div>
+                        <div>{{$store.state.currentUser.line_statusMessege}}</div>
+                      </div>
+                    </v-card-title>
+                  </v-flex>
                 </v-layout>
                 <v-divider light></v-divider>
                 <v-card-actions class="pa-3">
-                  Popularity
+                  Rate Popularity
                   <v-spacer></v-spacer>
                   <v-icon>star_border</v-icon>
                   <v-icon>star_border</v-icon>
@@ -41,13 +43,8 @@
             </v-flex>
           </v-layout>
         </v-container>
-      </v-card>            
-  </div>
-
-<!--
-  <div>
-
-<br>      
+      </v-card>      
+      <br>      
 displayName:
 {{$store.state.currentUser.line_displayName}}
 <br>
@@ -65,7 +62,13 @@ latitude:
 <br>
 longitude:
 {{$store.state.currentUser.longitude}}
-<br>
+<br>      
+  </div>
+
+<!--
+  <div>
+
+
   
   </div>
 -->
