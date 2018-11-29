8a892652593a167f8f31a221ba14aa68909e1b7b
ionflare <ccionflare@gmail.com>
2018-11-29 17:44:44 +0700

    test profile

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,7 +1,52 @@
 <template>
 
+
   <div>
-VUE FORM
+     <v-card>
+        <v-container
+          fluid
+          grid-list-lg
+        >
+          <v-layout row wrap>    
+
+    <v-flex xs12>
+              <v-card color="purple" class="white--text">
+                <v-layout row>
+                  <v-flex xs7>
+                    <v-card-title primary-title>
+                      <div>
+                        <div class="headline">{{$store.state.currentUser.line_displayName}}</div>
+                      </div>
+                    </v-card-title>
+                  </v-flex>
+                  <v-flex xs5>
+                    <v-img
+                      :src="$store.state.currentUser.line_pictureUrl"
+                      height="125px"
+                      contain
+                    ></v-img>
+                  </v-flex>
+                </v-layout>
+                <v-divider light></v-divider>
+                <v-card-actions class="pa-3">
+                  Popularity
+                  <v-spacer></v-spacer>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>            
+  </div>
+
+<!--
+  <div>
+
 <br>      
 displayName:
 {{$store.state.currentUser.line_displayName}}
@@ -23,7 +68,7 @@ longitude:
 <br>
   
   </div>
-
+-->
 </template>
 
 <script>
