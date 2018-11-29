88160900bcac9f87b81cf430ce2b3b4c227e01e1
ionflare <ccionflare@gmail.com>
2018-11-29 16:55:27 +0700

    add heroku url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,16 @@
 require("dotenv").config();
 const bodyParser = require('body-parser')
 const session = require('express-session')
-/*
+
  // setBaseURL
 var env = process.env.NODE_ENV || 'development';
 if (env === 'development' || env === 'test') {
-  //var API_URL='http://localhost:3000' //blank: localhost:3000
-  var API_URL= 'https://3c9df73438794ae49a6a317ae2eeb187.vfs.cloud9.ap-southeast-1.amazonaws.com'
-  
+  var API_URL='http://localhost:3000' //blank: localhost:3000
 }
 else{
   var API_URL=process.env.HEROKU_URL
 }
-*/
+
  
  
  module.exports = {
